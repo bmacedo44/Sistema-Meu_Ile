e9e96b7a502f8f2660acce5a01bd4554609418c2
Bernardo Macedo <bmacedo44@gmail.com>
2025-12-21 16:20:22 -0300

    Inclusão de cadastro usando Hash e lógica básica de validação (WIP: Falta incluir estruturas de repeticao como loop e while; vou incluir após estuda-las)

--- meu_ile.rb
+++ meu_ile.rb
@@ -1,3 +1,5 @@
+usuario_atual = {}
+
 puts "Exercício prático #001 - Ruby - Meu Ilé:"
 puts "____________________________________________________________"
 puts "Olá!"
@@ -11,51 +13,92 @@ puts "Carregando... (Por favor aguarde enquanto o sistema carrega a tela inicial
 sleep 4
 system "clear"
 
-puts "Por favor, insira o nome do seu Ilé Asè:"
-nome_casa = gets.chomp  # <--- VEJA: Colado na esquerda!
+puts "Você já possui cadastro no sistema?"
+puts "Digite (S) caso possua ou (N) caso não possua:"
+possui_cadastro = gets.chomp  
 
-puts "Por favor, me diga se é uma casa de Candomblé ou Umbanda:"
-nome_religiao = gets.chomp
+if possui_cadastro == "N"
 
-puts "Por favor, insira o nome da sacerdotisa ou sacerdote responsável pela casa:"
-nome_priest = gets.chomp
+    puts "Vamos criar seu cadastro, gentileza preencher os campos a seguir:"
 
-puts "Legal, #{nome_priest}! Pelo que entendi, você é responsável pela casa #{nome_casa}!"
-puts "Agora me diga, em que ano a casa #{nome_casa} foi fundada oficialmente?"
-data_fundacao = gets.chomp.to_i
+    puts "Digite seu nome de usuário: (Não são aceitos: acentos e caracteres especiais)"
+    usuario_atual[:user_name] = gets.chomp
 
-puts "Que legal! Isso que dizer que a casa #{nome_casa} possui #{Time.now.year - data_fundacao} anos! Que massa!"
-system "clear"
+    puts "Insira sua senha: (Sua senha deverá conter de 6 a 10 caracteres entre letras, números e caracteres especiais)"
+    usuario_atual[:user_password] = gets.chomp
 
-puts "Guardando as informações..."
-sleep 4
+    puts "Confirme sua senha:"
+    user_pwd_confirm = gets.chomp
+        if user_pwd_confirm != usuario_atual[:user_password]
+            puts "ERRO - As senhas não batem! Gentileza corrigir."
+            sleep 4
+        end
+    puts "Insira seu endereço de e-mail:"
+    usuario_atual[:user_email] = gets.chomp
 
-puts "Agora é a hora que o filho da casa até treme nas bases!"
-puts "Qual o valor da mensalidade cobrado atualmente?"
-print "R$ " # Adicionei um espaço depois do R$ para ficar bonito na tela
-valor_mensalidade = gets.chomp.to_f
+    puts "Confirme seu endereço de e=mail:"
+    user_email_confirmation = gets.chomp
+        if user_email_confirmation != usuario_atual[:user_email]
+            puts "ERRO - Endereço de e-mail não confere, gentileza corrigir"
+            sleep 4
+        end
 
-puts "Muito bom! Eu acho R$ #{valor_mensalidade} bem razoável!"
-system "clear"
+    puts "Por favor, insira o nome do seu Ilé Asè:"
+    usuario_atual[:nome_casa] = gets.chomp  
 
-puts "Por fim, me diga quantos integrantes ativos existem hoje na casa #{nome_casa}?"
-integrantes_ativos = gets.chomp.to_i
+    puts "Por favor, me diga se é uma casa de Candomblé ou Umbanda:"
+    usuario_atual[:nome_religiao] = gets.chomp
 
-puts "Ótimo! Agora já temos todas as informações iniciais para fazer o cadastro básico da casa #{nome_casa}!"
-puts "Por favor, aguarde enquanto estamos processando as informações."
-sleep 5
-system "clear"
+    puts "Por favor, insira o nome da sacerdotisa ou sacerdote responsável pela casa:"
+    usuario_atual[:nome_priest] = gets.chomp
 
-puts "Aguarde só mais um pouco, queremos ter certeza de que está tudo no seu devido lugar!"
-system "clear"
+    puts "Legal, #{usuario_atual[:nome_priest]}! Pelo que entendi, você é responsável pela casa #{usuario_atual[:nome_casa]}!"
+    puts "Agora me diga, em que ano a casa #{usuario_atual[:nome_casa]} foi fundada oficialmente?"
+    usuario_atual[:data_fundacao] = gets.chomp.to_i
 
-puts "Realizando os últimos ajustes..."
-sleep 6
+    puts "Que legal! Isso que dizer que a casa #{usuario_atual[:nome_casa]} possui #{Time.now.year - usuario_atual[:data_fundacao]} anos! Que massa!"
+    system "clear"
 
-puts "PRONTO! Cadastro realizado com sucesso!"
-sleep 3
-system "clear"
+    puts "Guardando as informações..."
+    sleep 4
 
-puts "A casa #{nome_casa} é comandada por #{nome_priest} e toca #{nome_religiao}"
-puts "A casa #{nome_casa} possui #{Time.now.year - data_fundacao} anos e conta com #{integrantes_ativos} integrantes ativos!"
-puts "Hoje é cobrado o valor simbólico de R$ #{valor_mensalidade} para auxiliar na manutenção e pagamento das contas básicas da casa."
+    puts "Agora é a hora que o filho da casa até treme nas bases!"
+    puts "Qual o valor da mensalidade cobrado atualmente?"
+    print "R$ " 
+    usuario_atual[:valor_mensalidade] = gets.chomp.to_f
+
+    puts "Muito bom! Eu acho R$ #{usuario_atual[:valor_mensalidade]} bem razoável!"
+    system "clear"
+
+    puts "Por fim, me diga quantos integrantes ativos existem hoje na casa #{usuario_atual[:nome_casa]}?"
+    usuario_atual[:integrantes_ativos] = gets.chomp.to_i
+
+    puts "Ótimo! Agora já temos todas as informações iniciais para fazer o cadastro básico da casa #{usuario_atual[:nome_casa]}!"
+    puts "Por favor, aguarde enquanto estamos processando as informações."
+    sleep 5
+    system "clear"
+
+    puts "Aguarde só mais um pouco, queremos ter certeza de que está tudo no seu devido lugar!"
+    system "clear"
+
+    puts "Realizando os últimos ajustes..."
+    sleep 6
+
+    puts "PRONTO! Cadastro realizado com sucesso!"
+    sleep 3
+    system "clear"
+
+    puts "A casa #{usuario_atual[:nome_casa]} é comandada por #{usuario_atual[:nome_priest]} e toca #{usuario_atual[:nome_religiao]}"
+    puts "A casa #{usuario_atual[:nome_casa]} possui #{Time.now.year - usuario_atual[:data_fundacao]} anos e conta com #{usuario_atual[:integrantes_ativos]} integrantes ativos!"
+    puts "Hoje é cobrado o valor simbólico de R$ #{usuario_atual[:valor_mensalidade]} para auxiliar na manutenção e pagamento das contas básicas da casa."
+
+end
+
+if possui_cadastro == "S"
+    puts "Por gentileza, insira aqui seu nome de usuário:"
+    autentication_user_name = gets.chomp
+    if autentication_user_name != usuario_atual[:user_name]
+        puts "ERRO - O usuário digitado não está cadastrado!"
+        sleep 4
+    end
+end  
